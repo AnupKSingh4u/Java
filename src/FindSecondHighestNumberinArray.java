9c5553334309f457435fdc6febe814bfc1d4728f
AnupKSingh4u <anupksingh4u@gmail.com>
2019-10-22 23:07:49 +0530

    removed on print command

--- src/FindSecondHighestNumberinArray.java
+++ src/FindSecondHighestNumberinArray.java
@@ -26,7 +26,7 @@ public class FindSecondHighestNumberinArray {
         	 }
          }
          
-         System.out.println();
+        
          System.out.println(Sec_highest);
          System.out.println(highest);
 	}
